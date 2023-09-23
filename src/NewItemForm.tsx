ca3353fb6487fd3be2500a32ab1e722678f9cbb2
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-23 17:41:05 +0545

    Auto focus on inputs

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "./styles";
+import { useFocus } from "./utils/useFocus";
 
 type Props = {
   onAdd(text: string): void;
 };
 const NewItemForm = ({ onAdd }: Props) => {
   const [text, setText] = useState("");
+  const inputRef = useFocus();
+
   return (
     <>
       <NewItemFormContainer>
-        <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+        <NewItemInput
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          ref={inputRef}
+        />
         <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
       </NewItemFormContainer>
     </>
