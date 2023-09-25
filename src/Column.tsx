4d8a405a2209f8de5a6343dc3f5e6dc20268a343
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-25 09:34:59 +0545

    drag column

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,19 +1,38 @@
+import { useRef } from "react";
 import AddNewItem from "./AddNewItem";
 import Card from "./Card";
 import { useAppState } from "./state/AppStateContext";
-import { addTask } from "./state/actions";
+import { addTask, moveList } from "./state/actions";
 import { ColumnContainer, ColumnTitle } from "./styles";
+import { useItemDrag } from "./utils/useItemDrag";
+import { useDrop } from "react-dnd";
+import { throttle } from "throttle-debounce-ts";
 
 type Props = {
   text: string;
   id: string;
 };
 const Column = ({ text, id }: Props) => {
-  const { getTasksByListId, dispatch } = useAppState();
+  const { draggedItem, getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useItemDrag({ type: "COLUMN", id, text });
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover: throttle(200, () => {
+      if (!draggedItem) return;
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) return;
+        dispatch(moveList(draggedItem.id, id));
+      }
+    }),
+  });
+
+  drag(drop(ref));
+
   return (
     <>
-      <ColumnContainer>
+      <ColumnContainer ref={ref}>
         <ColumnTitle>{text}</ColumnTitle>
         {tasks.map((task) => (
           <Card text={task.text} key={task.id} id={task.id} />
