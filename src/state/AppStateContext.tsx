5ae3dce36442b8ca93c1d08aa1b2d3670f3108b2
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-25 09:20:06 +0545

    store the dragged item in the STATE

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -2,6 +2,7 @@ import { Dispatch, createContext, useContext } from "react";
 import { AppState, List, Task, appStateReducer } from "./appStateReducer";
 import { Action } from "./actions";
 import { useImmerReducer } from "use-immer";
+import { DragItem } from "../DragItem";
 
 const appData: AppState = {
   lists: [
@@ -21,12 +22,14 @@ const appData: AppState = {
       tasks: [{ id: "t3", text: "Learn Happily Ever After" }],
     },
   ],
+  draggedItem: null,
 };
 
 type AppStateContextProps = {
   lists: List[];
   getTasksByListId(id: string): Task[];
   dispatch: Dispatch<Action>;
+  draggedItem: DragItem | null;
 };
 const AppStateContext = createContext<AppStateContextProps>(
   {} as AppStateContextProps
@@ -38,14 +41,16 @@ export const AppStateProvider = ({
   children: React.ReactNode;
 }) => {
   const [state, dispatch] = useImmerReducer(appStateReducer, appData);
-  const { lists } = state;
+  const { lists, draggedItem } = state;
 
   const getTasksByListId = (id: string) => {
     return lists.find((list) => list.id === id)?.tasks || [];
   };
 
   return (
-    <AppStateContext.Provider value={{ lists, getTasksByListId, dispatch }}>
+    <AppStateContext.Provider
+      value={{ lists, getTasksByListId, dispatch, draggedItem }}
+    >
       {children}
     </AppStateContext.Provider>
   );
